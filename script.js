91ac6d13cc34c948a54c53813b39bffb27dcc87c
Sudhir kumar <65527808+titanSlayer12@users.noreply.github.com>
2022-05-12 12:34:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const newYears = "7 Dec 2021";
+const newYears = "7 Dec 2022";
 
 function countdown() {
     const newYearsDate = new Date(newYears);
